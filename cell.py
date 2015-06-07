c0de7332c4cd403e29d8cc92f62b19b6848f1463
igdimitro <van2003@abv.bg>
2015-06-08 00:33:37 +0300

    not much changed

--- cell.py
+++ cell.py
@@ -1,3 +1,21 @@
 class cell:
 
-    pass
+    def __init__(self):
+        self.__coords = (0, 0)
+        self.__type = ""
+
+    @property
+    def coords(self):
+        return self.__coords
+
+    @coords.setter
+    def coords(self, value):
+        self.__coords = value
+
+    @property
+    def type(self):
+        return self.__type
+
+    @type.setter
+    def type(self, value):
+        self.__type = value
